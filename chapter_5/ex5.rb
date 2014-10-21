c4eb1059aed73e23f11b8fea5036f2833a2bf78f
Grace Yu <grace.yu4444@gmail.com>
2014-10-20 18:29:12 -0600

    add chapter_5

---
+++ chapter_5/ex5.rb
@@ -0,0 +1,29 @@
+name = 'Zad A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+centimeters_per_inches = 2.54
+weight = 180 # lbs
+kilograms_per_pounds = 0.453592
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "He's teeth is usually #{teeth} depends on the coffee."
+
+# This line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+puts "Let's talk about #{name}."
+puts "He's #{height * centimeters_per_inches} inches tall."
+puts "He's #{weight * kilograms_per_pounds} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "He's teeth is usually #{teeth} depends on the coffee."
+
+# This line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height * centimeters_per_inches}, and #{weight * kilograms_per_pounds} I get #{age + (height * centimeters_per_inches) + (weight * kilograms_per_pounds)}."
---
+++ chapter_5/readme.md
@@ -0,0 +1,3 @@
+>Can I make a variable like this: 1 = 'Zed Shaw'?
+
+  No, 1 is not a valid variable name. They need to start with a character, so a1 would work, but 1 will not. 
